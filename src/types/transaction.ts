9966eed89b54299b5076fea649058453a4c6ea27
nemo <nehemiah.blackburn@gmail.com>
2025-01-08 18:55:59 -0500

    token transfer interface

---
+++ src/types/transaction.ts
@@ -0,0 +1,27 @@
+export interface TokenTransfer {
+  tokenMint: string
+  from: string
+  to: string
+  amount: number
+}
+
+export interface TokenMetadata {
+  symbol: string
+  name: string
+  image: string
+  decimals: number
+}
+
+export interface EnrichedTokenTransfer extends TokenTransfer {
+  metadata?: TokenMetadata
+}
+
+export interface Transaction {
+  signature: string
+  timestamp: number
+  type: string
+  source: string
+  tokenTransfers?: TokenTransfer[]
+  enrichedTokenTransfers?: EnrichedTokenTransfer[]
+  // ... other fields
+} 
