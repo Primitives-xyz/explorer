faeb26f858e7899b032e18dd160b9da20f18ea89
Nicholas Oxford <maheledaily@gmail.com>
2025-02-02 08:32:38 -0700

    cache expirement

--- src/components/transactions/nft-transaction-view.tsx
+++ src/components/transactions/nft-transaction-view.tsx
@@ -115,7 +115,6 @@ export const NFTTransactionView = memo(function NFTTransactionView({
             setNftInfo(null)
             return
           }
-          console.log({ nftInfo: data })
           setNftInfo(data)
         } catch (parseError) {
           console.error('Failed to parse NFT data:', parseError)
