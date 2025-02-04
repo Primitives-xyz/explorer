e8801a02d70ddba24603b3ca7f867959f43ecf1b
nemo <nehemiah.blackburn@gmail.com>
2025-02-04 15:08:48 -0500

    update name of site

--- src/components/header/Title.tsx
+++ src/components/header/Title.tsx
@@ -4,7 +4,7 @@ export const Title = () => {
   return (
     <Link href="/" className="hover:opacity-80 transition-opacity">
       <h1 className="text-xl sm:text-2xl font-mono font-bold tracking-tight text-green-400 truncate">
-        {`>`} social_graph_explorer
+        {`>`} solana_social_explorer
       </h1>
     </Link>
   )
