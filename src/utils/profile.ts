2fd5da02843f777869fdbc635a8d1d4e322634dc
Marcus Smith <marcushsmith@comcast.net>
2025-02-05 14:16:30 -0800

    api

--- src/utils/profile.ts
+++ src/utils/profile.ts
@@ -18,7 +18,8 @@ export async function getProfileMetadata(
     console.log('NEXT_PUBLIC_VERCEL_URL', process.env.NEXT_PUBLIC_VERCEL_URL)
     console.log('NEXT_PUBLIC_APP_URL', process.env.NEXT_PUBLIC_APP_URL)
 
-    const baseUrl = process.env.VERCEL_URL || 'http://localhost:3000/api'
+    const baseUrl =
+      `${process.env.VERCEL_URL}/api` || 'http://localhost:3000/api'
 
     const url = `${baseUrl}/profiles/${username}`
     console.log('Fetching profile from URL:', url)
