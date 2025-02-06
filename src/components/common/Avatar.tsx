d3000184b214c573a03c0667463666d3c1c6bd34
Nicholas Oxford <maheledaily@gmail.com>
2025-02-06 11:48:27 -0700

    fix prop

--- src/components/common/Avatar.tsx
+++ src/components/common/Avatar.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 interface AvatarProps {
   username: string
   size?: number
-  imageUrl: string | null
+  imageUrl?: string | null
 }
 
 export function Avatar({ username, size = 32, imageUrl }: AvatarProps) {
