399557cbb8333d0c776905ef5fa9a995f289ca28
Cedrick <monsieur.cedrick@gmail.com>
2025-02-11 16:04:25 +0100

    huge refacto and translation

--- src/components/header-container/header.tsx
+++ src/components/header-container/header.tsx
@@ -1,11 +1,11 @@
 'use client'
 
 import { useCurrentWallet } from '@/components/auth/hooks/use-current-wallet'
-import { TerminalStatus } from '@/components/header/terminal-status'
-import { Title } from '@/components/header/title'
-import { UserActions } from '@/components/header/user-actions'
+import { TerminalStatus } from '@/components/header-container/terminal-status'
+import { Title } from '@/components/header-container/title'
+import { UserActions } from '@/components/header-container/user-actions'
 
-export const Header = () => {
+export function Header() {
   const { walletAddress, mainUsername } = useCurrentWallet()
 
   return (
