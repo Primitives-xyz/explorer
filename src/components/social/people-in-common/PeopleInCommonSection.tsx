1eb13630164a30abf47d1bc578b8d42df5d98f75
nemo <nehemiah.blackburn@gmail.com>
2025-02-11 18:18:06 -0800

    change token holders copy (#89)
    * try
    
    * ok
    
    * remove tokenname

--- src/components/social/people-in-common/PeopleInCommonSection.tsx
+++ src/components/social/people-in-common/PeopleInCommonSection.tsx
@@ -20,8 +20,8 @@ const UserLink = ({ username }: { username: string }) => (
 
 export const PeopleInCommonSection = ({
   topUsers,
-  totalAmount,
-  tokenName,
+  totalAmount
+  
 }: PeopleInCommonSectionProps) => {
   return (
     <div className="flex items-center gap-1">
@@ -39,23 +39,24 @@ export const PeopleInCommonSection = ({
       <span className="text-sm">
         {totalAmount > 3 ? (
           <>
+            owned by{' '}
             {topUsers.slice(0, 3).map((user, index) => (
               <>
                 <UserLink key={user.username} username={user.username} />
                 {index < 2 && ', '}
               </>
             ))}{' '}
-            and {totalAmount - 3} others you follow own {tokenName}
+            and {totalAmount - 3} others you follow
           </>
         ) : (
           <>
+            owned by{' '}
             {topUsers.map((user, index) => (
               <>
                 <UserLink key={user.username} username={user.username} />
                 {index < topUsers.length - 1 && ', '}
               </>
-            ))}{' '}
-            owns {tokenName}
+            ))}
           </>
         )}
       </span>
