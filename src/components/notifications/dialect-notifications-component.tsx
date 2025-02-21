942c99b2894084a4ac4e265fdf9681e6f6877761
Cedrick <monsieur.cedrick@gmail.com>
2025-02-21 16:27:07 +0100

    notifications

--- src/components/notifications/dialect-notifications-component.tsx
+++ src/components/notifications/dialect-notifications-component.tsx
@@ -4,11 +4,12 @@ import { DialectSolanaSdk } from '@dialectlabs/react-sdk-blockchain-solana'
 import { NotificationsButton } from '@dialectlabs/react-ui'
 import '@dialectlabs/react-ui/index.css'
 
-const DAPP_ADDRESS =
-  process.env.NEXT_PUBLIC_DAPP_ADDRESS ||
-  '2dCVckCxPgmTPass9sqYedLi9QNQc7yNuW6rPTU1Su4d'
+const DAPP_ADDRESS = process.env.NEXT_PUBLIC_DAPP_ADDRESS
 
 export const DialectNotificationsComponent = () => {
+  if (!DAPP_ADDRESS) {
+    return null
+  }
   return (
     <DialectSolanaSdk dappAddress={DAPP_ADDRESS}>
       <NotificationsButton />
