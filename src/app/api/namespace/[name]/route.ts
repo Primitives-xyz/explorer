954c2f329165418588594aaf6b9fcf7cf7c4415f
Marcus Smith <marcushsmith@comcast.net>
2025-02-24 10:29:27 -0800

    clean

--- src/app/api/namespace/[name]/route.ts
+++ src/app/api/namespace/[name]/route.ts
@@ -4,8 +4,6 @@ import { NextResponse } from 'next/server'
 export async function GET(request: Request) {
   const name = request.url.split('/').pop() as string
 
-  console.log('[name namespace]', name)
-
   const namespaceDetails = await contentServer.getNamespaceDetails({ name })
 
   return NextResponse.json({ namespaceDetails })
