5841a5c7bb94472605e78a423e919742ba083c5c
Marcus Smith <marcushsmith@comcast.net>
2025-02-25 10:20:27 -0800

    clean

--- src/components/notifications/dialect-notifications-component.tsx
+++ src/components/notifications/dialect-notifications-component.tsx
@@ -3,29 +3,15 @@
 import { DialectSolanaSdk } from '@dialectlabs/react-sdk-blockchain-solana'
 import { NotificationsButton } from '@dialectlabs/react-ui'
 import '@dialectlabs/react-ui/index.css'
-import {
-  ConnectionProvider,
-  WalletProvider,
-} from '@solana/wallet-adapter-react'
-import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets'
-import { clusterApiUrl } from '@solana/web3.js'
-import { useMemo } from 'react'
 
 const DAPP_ADDRESS =
   process.env.NEXT_PUBLIC_DAPP_ADDRESS ||
   '2dCVckCxPgmTPass9sqYedLi9QNQc7yNuW6rPTU1Su4d'
 
 export const DialectNotificationsComponent = () => {
-  const endpoint = useMemo(() => clusterApiUrl('mainnet-beta'), [])
-  const wallets = useMemo(() => [new PhantomWalletAdapter()], [])
-
   return (
-    <ConnectionProvider endpoint={endpoint}>
-      <WalletProvider wallets={wallets} autoConnect>
-        <DialectSolanaSdk dappAddress={DAPP_ADDRESS}>
-          <NotificationsButton />
-        </DialectSolanaSdk>
-      </WalletProvider>
-    </ConnectionProvider>
+    <DialectSolanaSdk dappAddress={DAPP_ADDRESS}>
+      <NotificationsButton />
+    </DialectSolanaSdk>
   )
 }
