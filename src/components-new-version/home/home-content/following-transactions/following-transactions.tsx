2839592b94bb16a3fa9c0a630c267a41afa08719
Cedrick <monsieur.cedrick@gmail.com>
2025-03-18 16:24:56 +0100

    feed

--- src/components-new-version/home/home-content/following-transactions/following-transactions.tsx
+++ src/components-new-version/home/home-content/following-transactions/following-transactions.tsx
@@ -4,6 +4,7 @@ import { FilterButton } from '@/components-new-version/home/home-content/followi
 import { useFollowingTransactions } from '@/components-new-version/home/home-content/following-transactions/hooks/use-following-transactions'
 import { TransactionsEntry } from '@/components-new-version/home/home-content/following-transactions/transactions-entry'
 import { useGetFollowing } from '@/components-new-version/tapestry/hooks/use-get-following'
+import { Spinner } from '@/components-new-version/ui'
 import { useCurrentWallet } from '@/components-new-version/utils/use-current-wallet'
 
 export function FollowingTransactions() {
@@ -24,6 +25,12 @@ export function FollowingTransactions() {
         selectedType={selectedType}
         setSelectedType={setSelectedType}
       />
+
+      {isLoadingTransactions && (
+        <div className="w-full flex justify-center items-center pt-24">
+          <Spinner large />
+        </div>
+      )}
       {aggregatedTransactions.map((transaction, index) => (
         <TransactionsEntry key={index} transaction={transaction} />
       ))}
