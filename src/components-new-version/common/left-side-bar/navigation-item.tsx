6949dfc0243b290d89721533abaf941706919bbc
AnotherRusty <nailrusty.dev@gmail.com>
2025-03-25 05:15:20 +0900

    add hover for sidebar button

--- src/components-new-version/common/left-side-bar/navigation-item.tsx
+++ src/components-new-version/common/left-side-bar/navigation-item.tsx
@@ -15,7 +15,7 @@ export default function NavigationItem({ title, icon: Icon, isActive = false, on
     <div
       className={clsx(
         "flex flex-row items-center gap-4 px-1 py-2 text-[#F5F8FD] text-[16px] font-bold leading-[150%] capitalize rounded-[6px] cursor-pointer transition-colors",
-        "md:px-4",
+        "md:px-4 hover:text-[#97EF83] hover:scale-105",
         isActive && "bg-[#97EF83] text-black/90",
       )}
       onClick={onClick}
