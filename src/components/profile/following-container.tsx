708ea99641bba8a414aa013be9d980f1493d1a44
Abr3u <rjlabreu@gmail.com>
2025-03-26 11:49:52 +0000

    works

--- src/components/profile/following-container.tsx
+++ src/components/profile/following-container.tsx
@@ -1,5 +1,7 @@
+import { useGetNamespaceProfiles } from '@/hooks/use-get-namespace-profiles'
 import { IProfile } from '@/types/profile.types'
 import { useIsLoggedIn } from '@dynamic-labs/sdk-react-core'
+import { useMemo } from 'react'
 import { FollowingTransactionFeed } from './following-transaction-feed'
 import { useFollowingTransactions } from './hooks/use-following-transactions'
 import { useGetFollowing } from './hooks/use-get-following'
@@ -25,32 +27,27 @@ export const ActivityFeedContainer = ({
     setSelectedType,
   } = useFollowingTransactions(following)
 
-  // console.log('following ------------')
-  // console.log(JSON.stringify(following, null, 2))
-
   // kolscan data
-  // const { data: kolscanData, isLoading: kolscanLoading } =
-  //   useGetNamespaceProfiles({
-  //     name: 'kolscan',
-  //   })
+  const { data: kolscanData, isLoading: kolscanLoading } =
+    useGetNamespaceProfiles({
+      name: 'kolscan',
+    })
 
-  // const kolscanTransactionsInput = {
-  //   page: kolscanData?.page || 1,
-  //   pageSize: kolscanData?.pageSize || 10,
-  //   profiles: kolscanData
-  //     ? (kolscanData.profiles
-  //         .flatMap((p) => ({
-  //           ...p.profile,
-  //           wallet: {
-  //             id: p.wallet.address,
-  //           },
-  //         }))
-  //         .slice(0, 1) as IProfile[])
-  //     : [], // Always provide at least an empty array
-  // }
-
-  // console.log('kolscanData ------------')
-  // console.log(JSON.stringify(kolscanTransactionsInput, null, 2))
+  // transform and memoize
+  const kolscanTransactionsInput = useMemo(() => {
+    return {
+      page: kolscanData?.page || 1,
+      pageSize: kolscanData?.pageSize || 10,
+      profiles: kolscanData
+        ? (kolscanData.profiles.flatMap((p) => ({
+            ...p.profile,
+            wallet: {
+              id: p.wallet.address,
+            },
+          })) as IProfile[])
+        : [],
+    }
+  }, [kolscanData])
 
   const {
     aggregatedTransactions: kolscanAggregatedTransactions,
@@ -59,23 +56,7 @@ export const ActivityFeedContainer = ({
     totalWallets: kolscanTotalWallets,
     selectedType: kolscanSelectedType,
     setSelectedType: kolscanSetSelectedType,
-  } = useFollowingTransactions({
-    page: 1,
-    pageSize: 10,
-    profiles: [
-      {
-        image:
-          'https://cdn.kolscan.io/profiles/3h65MmPZksoKKyEpEjnWU2Yk2iYT5oZDNitGy5cTaxoE.png',
-        namespace: 'kolscan',
-        created_at: 1741273608482,
-        id: 'Jidn',
-        username: 'Jidn',
-        wallet: {
-          id: '3h65MmPZksoKKyEpEjnWU2Yk2iYT5oZDNitGy5cTaxoE',
-        },
-      },
-    ] as IProfile[],
-  })
+  } = useFollowingTransactions(kolscanTransactionsInput)
 
   return (
     <div className="space-y-4">
@@ -89,10 +70,9 @@ export const ActivityFeedContainer = ({
         setSelectedType={setSelectedType}
       />
       <FollowingTransactionFeed
-        title="KOLScan"
+        title="Twitter KOLs"
         transactions={kolscanAggregatedTransactions}
-        isLoading={kolscanIsLoadingTransactions}
-        // isLoading={kolscanIsLoadingTransactions || kolscanLoading}
+        isLoading={kolscanIsLoadingTransactions || kolscanLoading}
         isLoggedIn={isLoggedIn}
         loadedWallets={kolscanLoadedWallets}
         totalWallets={kolscanTotalWallets}
