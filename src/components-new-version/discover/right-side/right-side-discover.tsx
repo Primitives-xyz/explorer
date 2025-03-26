4bcc5a2aa7e9c28f3ae325d9d8e1b6fe3bf3f920
Cedrick <monsieur.cedrick@gmail.com>
2025-03-26 15:59:37 +0100

    discover

--- src/components-new-version/discover/right-side/right-side-discover.tsx
+++ src/components-new-version/discover/right-side/right-side-discover.tsx
@@ -1,65 +1,80 @@
 'use client'
 
 import { FollowButton } from '@/components-new-version/common/follow-button'
-import { DataTable } from '@/components-new-version/common/table/data-table'
 import { WalletAddressButton } from '@/components-new-version/common/wallet-address-button'
-import { IGetProfilesResponseEntry } from '@/components-new-version/models/profiles.models'
 import { useGetAllProfiles } from '@/components-new-version/tapestry/hooks/use-get-all-profiles'
-import { Button, ButtonVariant } from '@/components-new-version/ui'
+import {
+  Button,
+  ButtonVariant,
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+  Spinner,
+} from '@/components-new-version/ui'
 import { Avatar } from '@/components-new-version/ui/avatar/avatar'
 import { route } from '@/components-new-version/utils/route'
 import { useCurrentWallet } from '@/components-new-version/utils/use-current-wallet'
-import { ColumnDef } from '@tanstack/react-table'
+import { cn } from '@/components-new-version/utils/utils'
 
 export function RightSideDiscover() {
   const { profiles, loading } = useGetAllProfiles()
   const { mainUsername } = useCurrentWallet()
 
-  const columns: ColumnDef<IGetProfilesResponseEntry>[] = [
-    {
-      id: 'profile.username',
-      header: 'Recent Profiles',
-      enableSorting: false,
-      cell: ({ row }) => {
-        const { wallet, profile } = row.original
-        const isSame = wallet.address === profile.username
-
-        return (
-          <span className="flex items-center justify-between">
-            <div className="flex items-center gap-4">
-              <Avatar size={32} username={profile.username} />
-              <div className="flex flex-col">
-                {!isSame && (
-                  <Button
-                    variant={ButtonVariant.GHOST}
-                    href={route('address', { id: profile.username })}
-                    className="p-0 w-fit hover:bg-transparent"
-                  >
-                    @{profile.username}
-                  </Button>
-                )}
-                <WalletAddressButton walletAddress={wallet.address} />
-              </div>
-            </div>
-            <FollowButton
-              small
-              followerUsername={mainUsername}
-              followeeUsername={row.original.profile.username}
-            />
-          </span>
-        )
-      },
-    },
-  ]
-
   return (
     <div className="pt-[100px] space-y-4 flex flex-col">
-      <DataTable
-        data={profiles?.profiles || []}
-        columns={columns}
-        isLoading={loading}
-        withPagination
-      />
+      <Card>
+        <CardHeader className="!bg-card-accent !rounded-t-lg p-4 text-muted">
+          <CardTitle>Recent profiles</CardTitle>
+        </CardHeader>
+        <CardContent className="p-0 max-h-[calc(100vh-200px)] overflow-auto">
+          <>
+            {loading && (
+              <span className="flex justify-center items-center h-48">
+                <Spinner />
+              </span>
+            )}
+
+            {profiles?.profiles.map((elem) => {
+              const isSame = elem.wallet.address === elem.profile.username
+
+              return (
+                <span
+                  className={cn(
+                    'flex items-center justify-between p-4',
+                    profiles?.profiles.indexOf(elem) !==
+                      profiles?.profiles.length - 1 &&
+                      'border-b border-card-border'
+                  )}
+                >
+                  <div className="flex items-center gap-4">
+                    <Avatar size={32} username={elem.profile.username} />
+                    <div className="flex flex-col">
+                      {!isSame && (
+                        <Button
+                          variant={ButtonVariant.GHOST}
+                          href={route('address', { id: elem.profile.username })}
+                          className="p-0 w-fit hover:bg-transparent"
+                        >
+                          @{elem.profile.username}
+                        </Button>
+                      )}
+                      <WalletAddressButton
+                        walletAddress={elem.wallet.address}
+                      />
+                    </div>
+                  </div>
+                  <FollowButton
+                    small
+                    followerUsername={mainUsername}
+                    followeeUsername={elem.profile.username}
+                  />
+                </span>
+              )
+            })}
+          </>
+        </CardContent>
+      </Card>
     </div>
   )
 }
