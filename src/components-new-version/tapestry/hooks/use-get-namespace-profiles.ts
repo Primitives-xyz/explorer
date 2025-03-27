94f291803d0277835bfe2444a9e83c763217ad33
Abr3u <rjlabreu@gmail.com>
2025-03-27 09:59:55 +0000

    remove unused

--- src/components-new-version/tapestry/hooks/use-get-namespace-profiles.ts
+++ src/components-new-version/tapestry/hooks/use-get-namespace-profiles.ts
@@ -1,15 +1,6 @@
 import { IGetProfilesResponse } from '@/components-new-version/models/profiles.models'
 import { useQuery } from '@/components-new-version/utils/api'
 
-async function fetchNamespaceProfiles(url: string) {
-  const res = await fetch(url)
-  if (!res.ok) {
-    const errorData = await res.json()
-    throw new Error(errorData.error || 'Failed to fetch namespace profiles')
-  }
-  return await res.json()
-}
-
 export const useGetNamespaceProfiles = ({ name }: { name: string }) => {
   const { data, loading, error, refetch } = useQuery<IGetProfilesResponse>({
     endpoint: `profiles?namespace=${name}`,
