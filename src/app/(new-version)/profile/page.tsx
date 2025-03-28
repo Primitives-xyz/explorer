cd9c9f2a4525972c6f96dafc1a9d2319df2375c6
Cedrick <monsieur.cedrick@gmail.com>
2025-03-28 15:18:37 +0100

    profile page

--- src/app/(new-version)/profile/page.tsx
+++ src/app/(new-version)/profile/page.tsx
@@ -2,17 +2,11 @@ import { OverflowContentWrapper } from '@/components-new-version/common/overflow
 import { RightSideLayout } from '@/components-new-version/common/right-side-layout'
 import { ProfileContent } from '@/components-new-version/profile/profile-content'
 
-interface ProfilePageProps {
-  params: { id: string }
-}
-
-export default function ProfilePage({ params }: ProfilePageProps) {
-  const { id } = params
-
+export default function Profile() {
   return (
     <>
       <OverflowContentWrapper>
-        <ProfileContent id={id} />
+        <ProfileContent id="cedrick" />
       </OverflowContentWrapper>
       <RightSideLayout>
         <p>right</p>
