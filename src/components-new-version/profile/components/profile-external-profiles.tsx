69d15ce9c22f793eece2c77856e42b0432bfaee6
Paul Giron <pol.giron@gmail.com>
2025-04-14 12:38:20 +0200

    better external profiles

--- src/components-new-version/profile/components/profile-external-profiles.tsx
+++ src/components-new-version/profile/components/profile-external-profiles.tsx
@@ -2,9 +2,9 @@
 
 import { useGetIdentities } from '@/components-new-version/tapestry/hooks/use-get-identities'
 import { Button, ButtonVariant, Spinner } from '@/components-new-version/ui'
+import { Avatar } from '@/components-new-version/ui/avatar/avatar'
 import { EXPLORER_NAMESPACE } from '@/components-new-version/utils/constants'
 import { ExternalLinkIcon } from 'lucide-react'
-import Image from 'next/image'
 
 interface Props {
   walletAddress: string
@@ -24,41 +24,61 @@ export function ProfileExternalProfiles({ walletAddress }: Props) {
   }
 
   return (
-    <div className="space-y-2">
+    <div className="divide-y divide-border">
       {identities?.map((identity, index) => (
-        <Button
+        <div
           key={`${identity.namespace.name}-${identity.profile.username}-${index}`}
-          href={
-            identity.namespace.name === EXPLORER_NAMESPACE
-              ? `/${identity.profile.username}`
-              : `/n/${identity.namespace.name}/${identity.profile.username}`
-          }
-          className="w-full h-auto gap-2 px-2.5"
-          variant={ButtonVariant.GHOST}
+          className="py-2"
         >
-          <div className="relative shrink-0 w-8 aspect-square rounded-full overflow-hidden bg-muted flex items-center justify-center">
-            {identity.namespace.faviconURL ? (
-              <Image
-                src={identity.namespace.faviconURL}
-                alt={identity.namespace.readableName}
-                width={32}
-                height={32}
-                className="w-full h-full object-cover"
-              />
-            ) : (
-              <span className="text-primary text-lg">
-                {identity.namespace.readableName.charAt(0)}
-              </span>
-            )}
-          </div>
-          <div className="flex-1">
-            <div className="text-sm truncate">{identity.profile.username}</div>
-            <div className="text-xs truncate text-muted-foreground">
-              {identity.namespace.readableName}
+          <Button
+            href={
+              identity.namespace.name === EXPLORER_NAMESPACE
+                ? `/${identity.profile.username}`
+                : `/n/${identity.namespace.name}/${identity.profile.username}`
+            }
+            className="w-full h-auto gap-2 px-2.5 items-start"
+            variant={ButtonVariant.GHOST}
+          >
+            {/* <div className="relative shrink-0 w-8 aspect-square rounded-full overflow-hidden bg-muted flex items-center justify-center">
+              {identity.namespace.faviconURL ? (
+                <Image
+                  src={identity.namespace.faviconURL}
+                  alt={identity.namespace.readableName}
+                  width={32}
+                  height={32}
+                  className="w-full h-full object-cover"
+                />
+              ) : (
+                <span className="text-primary text-lg">
+                  {identity.namespace.readableName.charAt(0)}
+                </span>
+              )}
+            </div> */}
+            <Avatar
+              imageUrl={identity.profile.image}
+              username={identity.profile.username}
+            />
+            <div className="flex-1 w-full overflow-hidden flex flex-col gap-1">
+              <div className="text-sm truncate">
+                {identity.profile.username}
+              </div>
+              {!!identity.profile.bio && (
+                <div className="text-xs truncate font-light">
+                  {identity.profile.bio}
+                </div>
+              )}
+              <p className="text-xs text-muted-foreground font-light">
+                {identity.socialCounts?.followers ?? 0} Followers •{' '}
+                {identity.socialCounts?.following ?? 0} Following •{' '}
+                {identity.namespace.readableName}
+              </p>
             </div>
-          </div>
-          <ExternalLinkIcon className="text-muted-foreground" size={16} />
-        </Button>
+            <ExternalLinkIcon
+              className="text-muted-foreground ml-10"
+              size={16}
+            />
+          </Button>
+        </div>
       ))}
     </div>
   )
