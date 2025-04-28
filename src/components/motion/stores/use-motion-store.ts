810156f675c18e2ee77ab9f078842e08e072813c
Paul Giron <pol.giron@gmail.com>
2025-04-28 17:40:22 +0200

    save animation toggle in local storage

--- src/components/motion/stores/use-motion-store.ts
+++ src/components/motion/stores/use-motion-store.ts
@@ -1,14 +1,22 @@
 import { create } from 'zustand'
+import { persist } from 'zustand/middleware'
 
 interface State {
   enableMotion: boolean
   setEnableMotion: (enable: boolean) => void
 }
 
-export const useMotionStore = create<State>()((set) => ({
-  enableMotion: false,
-  setEnableMotion: (enableMotion) =>
-    set(() => ({
-      enableMotion,
-    })),
-}))
+export const useMotionStore = create(
+  persist<State>(
+    (set) => ({
+      enableMotion: true,
+      setEnableMotion: (enableMotion) =>
+        set(() => ({
+          enableMotion,
+        })),
+    }),
+    {
+      name: 'enable-motion',
+    }
+  )
+)
