38480092880af468a29484ebfa857735d1f51321
David Mackay <david.mackay@usetapestry.dev>
2025-04-28 23:08:13 -0400

    current state pre memory migration

---
+++ src/app/stream/page.tsx
@@ -0,0 +1,10 @@
+import { MainContentWrapper } from '@/components/common/main-content-wrapper'
+import { StreamContent } from '@/components/stream/stream-content'
+
+export default function Stream() {
+  return (
+    <MainContentWrapper>
+      <StreamContent />
+    </MainContentWrapper>
+  )
+} 
