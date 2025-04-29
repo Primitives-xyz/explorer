bc3076f59be0406ea69b5a45b56076c9f7c297b2
Cedrick <monsieur.cedrick@gmail.com>
2025-04-29 14:42:49 +0200

    twitter feed

--- src/components/profile/components/twitter-feed.tsx
+++ src/components/profile/components/twitter-feed.tsx
@@ -1,5 +1,16 @@
 import { useGetXFeed } from '@/components/tapestry/hooks/use-get-x-feed'
 import { IGetProfilesResponseEntry } from '@/components/tapestry/models/profiles.models'
+import {
+  Button,
+  ButtonSize,
+  ButtonVariant,
+  Card,
+  CardContent,
+  CardHeader,
+  Spinner,
+} from '@/components/ui'
+import { formatSmartNumber } from '@/utils/formatting/format-number'
+import { ExternalLink, Heart, IterationCw } from 'lucide-react'
 
 interface Props {
   profile: IGetProfilesResponseEntry
@@ -9,22 +20,56 @@ export function TwitterFeed({ profile }: Props) {
     xUsername: profile.profile.username,
   })
 
-  console.log({ data })
-
-  if (loading) return <p>Loading tweets...</p>
-  if (error) return <p className="text-destructive">Error fetching tweets</p>
-  if (!data?.tweets?.length) return <p>No tweets found</p>
-
   return (
-    <div className="space-y-4">
-      {/* {data.tweets.map((tweet) => (
-        <div key={tweet.id} className="p-4 border rounded bg-muted">
-          <p className="whitespace-pre-wrap text-sm">{tweet.text}</p>
-          <p className="text-xs mt-1">
-            {new Date(tweet.created_at).toLocaleString()}
-          </p>
-        </div>
-      ))} */}
+    <div className="w-full flex flex-col items-center justify-center mt-10 space-y-5">
+      {loading && <Spinner />}
+      {error && <p className="text-xs">Error fetching tweets</p>}
+      {!data?.tweets?.length && <p className="text-xs">No tweets found</p>}
+      <div className="flex flex-col gap-3 w-full max-h-[calc(100vh-500px)] overflow-auto">
+        {data?.tweets.map((tweet) => (
+          <div key={tweet.id}>
+            <Card>
+              <CardHeader>
+                <div className="flex justify-between">
+                  <p>{data.handle}</p>
+                  <Button
+                    variant={ButtonVariant.LINK}
+                    size={ButtonSize.ICON}
+                    href={tweet.url}
+                    newTab
+                  >
+                    <ExternalLink size={16} />
+                  </Button>
+                </div>
+              </CardHeader>
+              <CardContent>
+                <div className="flex flex-col space-y-5">
+                  <p className="whitespace-pre-wrap text-sm">{tweet.text}</p>
+                  <div className="flex justify-between items-end">
+                    <div className="flex gap-6">
+                      <span className="text-xs text-muted-foreground flex items-center gap-1">
+                        <Heart size={12} />{' '}
+                        {formatSmartNumber(tweet.likes, {
+                          minimumFractionDigits: 0,
+                        })}
+                      </span>
+                      <span className="text-xs text-muted-foreground flex items-center gap-1">
+                        <IterationCw size={12} />{' '}
+                        {formatSmartNumber(tweet.retweets, {
+                          minimumFractionDigits: 0,
+                        })}
+                      </span>
+                    </div>
+                    <p className="text-xs text-muted-foreground">
+                      {new Date(tweet.createdAt).toLocaleString()}
+                    </p>
+                  </div>
+                </div>
+              </CardContent>
+            </Card>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
