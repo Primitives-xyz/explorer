fcb0520d19898e922c8a1df15b474a1369c18b3c
Paul Giron <pol.giron@gmail.com>
2025-04-30 09:58:58 +0200

    show the swap tray

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 'use client'
 
 import { MainContentWrapper } from '@/components/common/main-content-wrapper'
+import { RightSidebarWrapper } from '@/components/common/right-sidebar-wrapper'
 import { HomeContent } from '@/components/home/home-content/home-content'
+import { SwapTray } from '@/components/swap/components/swap-tray'
 import { FullPageSpinner } from '@/components/ui'
 import { useIsMobile } from '@/utils/use-is-mobile'
 import { useRouter } from 'next/navigation'
@@ -26,9 +28,9 @@ export default function Home() {
       <MainContentWrapper className="md:min-w-main-content md:max-w-main-content mx-auto flex justify-center">
         <HomeContent />
       </MainContentWrapper>
-      {/* <RightSidebarWrapper>
+      <RightSidebarWrapper>
         <SwapTray isAlwaysOpen />
-      </RightSidebarWrapper> */}
+      </RightSidebarWrapper>
     </>
   )
 }
