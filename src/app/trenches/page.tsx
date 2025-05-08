e9a17141b6b22d896fbab17b384d813ddae9d7d0
nemo <nehemiah.blackburn@gmail.com>
2025-05-08 14:49:26 -0400

    fix

--- src/app/trenches/page.tsx
+++ src/app/trenches/page.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import { MainContentWrapper } from '@/components/common/main-content-wrapper'
-import { StreamContent } from '@/components/stream/stream-content'
 import { SwapTray } from '@/components/swap/components/swap-tray'
 import { MobileSwapTray } from '@/components/swap/components/mobile-swap-tray'
 import { useIsMobile } from '@/utils/use-is-mobile'
