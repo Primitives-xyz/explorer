4fc6586b85303e9ec9cb9aea6c6d829748b99078
Paul Giron <pol.giron@gmail.com>
2025-05-23 16:00:41 +0200

    add stagger to solid score animation children

--- src/components/solid-score/animation/reveal-score-text.tsx
+++ src/components/solid-score/animation/reveal-score-text.tsx
@@ -1,5 +1,6 @@
 import { Animate, Button, ButtonSize } from '@/components/ui'
-import { ArrowRightIcon } from 'lucide-react'
+import { ArrowRightIcon } from '@dynamic-labs/sdk-react-core'
+import { AnimationProps, motion } from 'framer-motion'
 
 interface Props {
   open: boolean
@@ -7,33 +8,75 @@ interface Props {
 }
 
 export function RevealScoreText({ open, closeModal }: Props) {
+  const containerAnimationVariants: AnimationProps['variants'] = {
+    visible: {
+      transition: {
+        staggerChildren: 0.1,
+        delayChildren: 0.7,
+      },
+    },
+  }
+
+  const itemAnimationVariants: AnimationProps['variants'] = {
+    hidden: {
+      opacity: 0,
+      x: -80,
+    },
+    visible: {
+      opacity: 1,
+      x: 0,
+      // transition: {
+      //   type: 'spring',
+      //   stiffness: 200,
+      //   damping: 10,
+      // },
+      transition: {
+        duration: 0.4,
+        // delay: 0.7,
+        ease: [0.34, 1.56, 0.64, 1],
+      },
+    },
+    exit: {
+      opacity: 0,
+      x: 50,
+    },
+  }
+
   return (
     <Animate
       isVisible={open}
-      initial={{
-        opacity: 0,
-        x: -80,
-      }}
-      animate={{
-        opacity: 1,
-        x: 0,
-        transition: {
-          duration: 0.4,
-          delay: 0.7,
-          ease: [0.34, 1.56, 0.64, 1],
-        },
-      }}
-      exit={{
-        opacity: 0,
-        x: 50,
-      }}
-      className="flex flex-col items-center justify-center relative"
+      // initial={{
+      //   opacity: 0,
+      //   x: -80,
+      // }}
+      // animate={{
+      //   opacity: 1,
+      //   x: 0,
+      //   transition: {
+      //     duration: 0.4,
+      //     delay: 0.7,
+      //     ease: [0.34, 1.56, 0.64, 1],
+      //   },
+      // }}
+      // exit={{
+      //   opacity: 0,
+      //   x: 50,
+      // }}
+      variants={containerAnimationVariants}
+      initial="hidden"
+      animate="visible"
+      exit="exit"
+      className="flex flex-col items-start justify-center gap-2 relative"
     >
-      <div className="flex flex-col justify-center items-start gap-2">
+      <motion.div variants={itemAnimationVariants}>
         <div className="text-black/60 text-6xl font-semibold">
           Solid Score Unlocked!
         </div>
+      </motion.div>
+      <motion.div variants={itemAnimationVariants}>
         <div className="text-black/80 text-9xl font-bold">2,000</div>
+      </motion.div>
+      <motion.div variants={itemAnimationVariants}>
         <Button
           size={ButtonSize.LG}
           className="bg-black/80 hover:bg-black/90 text-white mt-5"
@@ -41,7 +84,7 @@ export function RevealScoreText({ open, closeModal }: Props) {
         >
           Go To Leaderboard <ArrowRightIcon />
         </Button>
-      </div>
+      </motion.div>
     </Animate>
   )
 }
